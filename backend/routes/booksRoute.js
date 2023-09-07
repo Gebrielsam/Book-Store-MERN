ab35b22dfb842ae2e6ffe79d2875f778c905f258
Onah Obinna Samuel <samgb042@gmail.com>
2023-09-07 10:08:13 +0100

    Final commit

--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -35,7 +35,7 @@ router.get('/', async (req, res) => {
 
         return res.status(200).json({
             count: books.length,
-            data: books
+            data: books,
         });
     } catch (error) {
         console.log(error);
